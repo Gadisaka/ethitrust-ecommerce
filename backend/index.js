796ecbd000d8b99ef09e42a5ee5f43c99f46c24b
gadisaka <gadisaka04@gmail.com>
2026-05-23 17:14:41 +0300

    kl

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const defaultOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:8080",
   "http://192.168.56.1:8080",
-  "https://ethitrust-ecommerce.vercel.app/",
+  "https://ethitrust-ecommerce.vercel.app",
 ];
 const extraOrigins = process.env.CORS_ORIGINS
   ? process.env.CORS_ORIGINS.split(",")
