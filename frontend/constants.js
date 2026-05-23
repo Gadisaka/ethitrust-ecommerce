c22f90786978cb84630c2cc82488aa2b559a5a39
gadisaka <gadisaka04@gmail.com>
2026-05-23 18:05:05 +0300

    Update API_URL in constants.js to point to the new Eco API endpoint, while commenting out the previous Ethitrust eCommerce URL for clarity.

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 // export const API_URL = "http://localhost:4000/api";
 
-// export const API_URL = "https://mule-mobile-backend.onrender.com/api";
+export const API_URL = "https://eco.api.sheqaygames.com/api";
 // export const API_URL = "/api";
-export const API_URL = "https://ethitrust-ecommerce.onrender.com/api";
+// export const API_URL = "https://ethitrust-ecommerce.onrender.com/api";
